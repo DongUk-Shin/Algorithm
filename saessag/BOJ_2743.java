d6abaff9791c375639067a24ad471eda384770ca
Donguk Shin <az9170@naver.com>
2024-02-06 17:27:07 +0900

    백준

---
+++ saessag/BOJ_2743.java
@@ -0,0 +1,16 @@
+package saessag;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class BOJ_2743 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s.length());
+
+		sc.close();
+	}
+}
