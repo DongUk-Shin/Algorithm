573bb090dc3c11d6f679ebff5f402428a0d8851d
Donguk Shin <az9170@naver.com>
2024-02-10 21:35:01 +0900

    백준

--- code/BOJ_.java
+++ code/BOJ_.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class BOJ_ {
 
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
