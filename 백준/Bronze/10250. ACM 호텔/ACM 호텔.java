1798894dbb627d85e10fe3231ce7f3c9e8883c1d
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2024-04-09 12:51:13 +0900

    [Bronze III] Title: ACM 호텔, Time: 140 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < t; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int[] numbers = new int[inputs.length];
+            
+            for (int q = 0; q < inputs.length; q++) {
+                numbers[q] = Integer.parseInt(inputs[q]);
+            }
+            
+            int h = numbers[0];
+            int w = numbers[1];
+            int n = numbers[2];
+            
+            int result = 0;
+            
+            if (n % h == 0) {
+                result = (h * 100) + (n / h);
+            } else {
+                result = ((n % h) * 100) + ((n / h) + 1);
+            }
+            
+            
+            System.out.println(result);
+        }
+        
+        
+    }
+}
