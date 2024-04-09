6b41366e6be71786ebe840f4b3b0ff3b098f35f8
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2024-04-09 12:50:54 +0900

    [Silver V] Title: 셀프 넘버, Time: 160 ms, Memory: 14592 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		boolean[] arr = new boolean[10001];
+
+
+		for (int i = 1; i <= 10001; i++) {
+			int n = d(i);
+			if (n < 10001) {
+				arr[n] = true;
+			}
+		}
+
+		for (int i = 1; i < 10001; i++) {
+			if (arr[i] == false) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static int d(int number) {
+		int s = number;
+
+		while (number > 0) {
+			s = s + (number % 10);
+			number /= 10;
+		}
+
+		return s;
+	}
+}
