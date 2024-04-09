514f563791fe2748b44e89b623eb9de8908e365a
DongUk <55849838+DongUk-Shin@users.noreply.github.com>
2024-04-09 12:49:52 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] numbers = new int[9];
+
+		for(int i = 0 ; i < 9 ; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		}
+
+		int max = 0, maxi = 0;
+
+		for (int i = 0; i < 9; i++) {
+			if (max < numbers[i]) {
+				max = numbers[i];
+				maxi = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(maxi + 1);
+
+	}
+}
